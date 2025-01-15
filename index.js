3e8d0e5c6f7f1dd2a499dad6418a54bf0e9e7610
Martin Dzhonov <martin.dzhonov@gmail.com>
2025-01-15 03:36:33 +0200

    strategem endpoint, missions fix

--- index.js
+++ index.js
@@ -2,15 +2,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 const mongoPass = encodeURIComponent('Crtstr#21')
-
 mongoose.connect(`mongodb+srv://martindzhonov:${mongoPass}@serverlessinstance0.hrhcm0l.mongodb.net/hd`)
-
-app.use(express.json());
-
-const port = process.env.PORT || 8080;
-
 const gameSchema = new mongoose.Schema({
     id: Number,
     faction: String,
@@ -21,11 +14,11 @@ const gameSchema = new mongoose.Schema({
     players: [],
     modifiers: [],
 })
-const GameModel = mongoose.model("game", gameSchema);
-const GameModel1 = mongoose.model("matches", gameSchema);
-
+const GameModel = mongoose.model("matches", gameSchema);
 const { factions, patchPeriods, missionModifiers, missionNames, strategems } = require('./constants');
 
+app.use(express.json());
+const port = process.env.PORT || 8080;
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -43,25 +36,58 @@ app.get('/test', (req, res) => {
     res.send('Welcome to my server!');
 });
 
-app.get('/faction/all', (req, res) => {
-    GameModel.find({}).then(function (games) {
+app.get('/faction/:id', (req, res) => {
+    const factionName = req.params['id'];
+    const patch = patchPeriods[0];
+    const options = factionName === "all" ? {} : {
+        faction: factionName,
+        createdAt: {
+            $gte: new Date(patch.start),
+            $lte: patch.end.toLowerCase() === 'present' ? new Date() : new Date(patch.end)
+        }
+    }
+    GameModel.find(options).then(function (games) {
         res.send(games);
     });
 });
 
-app.get('/faction/:id', (req, res) => {
-    const factionName = req.params['id'];
-    GameModel.find({ faction: factionName }).then(function (games) {
+app.get('/games/:faction/:id', (req, res) => {
+    const factionName = req.params['faction'];
+    const strategemName = req.params['id'];
+
+    GameModel.find({
+        faction: factionName,
+        'players': {
+            $elemMatch: { $elemMatch: { $in: [strategemName] } }
+        }
+    }).then(function (games) {
         res.send(games);
     });
+
+});
+
+app.get('/strategem', async (req, res) => {
+    console.time('Execution Time');
+
+    const patchesData = await Promise.all(patchPeriods.map(patch => GameModel.find({
+        createdAt: {
+            $gte: new Date(patch.start),
+            $lte: patch.end.toLowerCase() === 'present' ? new Date() : new Date(patch.end)
+        }
+    })))
+
+    const result = patchesData.map((patchData) => {
+        return getFactionData(patchData);
+    })
+    console.timeEnd('Execution Time');
+
+    res.send(result);
 });
 
 const getMissionsByLength = (type) => {
     return type === "All"
-        ? missionNames
-        : type === "Long"
-            ? missionNames.slice(0, 14)
-            : missionNames.slice(14, missionNames.length);
+        ? missionNames[0].concat(missionNames[1])
+        : type === "Long" ? missionNames[0] : missionNames[1];
 };
 
 const getMissionLength = (missionName) => {
@@ -76,6 +102,8 @@ const getFactionsDict = () => {
         acc[faction] = {
             totalGames: 0,
             totalLoadouts: 0,
+            missions: { short: 0, long: 0 },
+            diffs: { 10: 0, 9: 0, 8: 0, 7: 0 },
             strategems: strategemNames.reduce((acc, strategem) => {
                 acc[strategem] = {
                     loadouts: 0,
@@ -96,7 +124,7 @@ const getFactionsDict = () => {
 }
 
 const getItemsByCategory = (companions) => {
-    const sorted = Object.entries(companions).sort(([, a], [, b]) => b - a).map((item)=> { return {name: item[0], value: item[1]}})
+    const sorted = Object.entries(companions).sort(([, a], [, b]) => b - a).map((item)=> { return {name: item[0], total: item[1]}})
 
     return {
         all: sorted.slice(0, 4),
@@ -120,9 +148,11 @@ function getFactionData(patchData) {
         const uniqueItems = new Set(game.players.flat());
         const factionData = factionsDict[game.faction];
         factionData.totalGames++;
-
+    
         game.players.forEach((loadout) => {
             factionData.totalLoadouts++;
+            factionData.diffs[game.difficulty]++;
+            factionData.missions[getMissionLength(game.mission)]++;
 
             loadout.forEach((item) => {
                 const strategem = factionData.strategems[item];
@@ -155,49 +185,18 @@ function getFactionData(patchData) {
         for (const strategemKey in strategems) {
             const companions = strategems[strategemKey].companions;
             strategems[strategemKey].companions = getItemsByCategory(companions);
-            
+
             const modifiers = strategems[strategemKey].modifiers;
             strategems[strategemKey].modifiers = Object.fromEntries(
                 Object.entries(modifiers).filter(([key, value]) => value !== 0)
             );
         }
+        const sorted = Object.fromEntries(Object.entries(strategems).sort(([, a], [, b]) => b.loadouts - a.loadouts));
+        factionsDict[key].strategems = sorted;
     }
     
-    return factionsDict;
+    return  factionsDict;
 }
-
-app.get('/strategem', async (req, res) => {
-    console.time('Execution Time');
-
-    const patchesData = await Promise.all(patchPeriods.map(patch => GameModel1.find({
-        createdAt: {
-            $gte: new Date(patch.start),
-            $lte: patch.end.toLowerCase() === 'present' ? new Date() : new Date(patch.end)
-        }
-    })))
-
-    const result = patchesData.map((patchData) => {
-        return getFactionData(patchData);
-    })
-    console.timeEnd('Execution Time');
-
-    res.send(result);
-});
-
-app.get('/games/:faction/:id', (req, res) => {
-    const factionName = req.params['faction'];
-    const strategemName = req.params['id'];
-
-    GameModel.find({
-        faction: factionName,
-        'players': {
-            $elemMatch: { $elemMatch: { $in: [strategemName] } }
-        }
-    }).then(function (games) {
-        res.send(games);
-    });
-
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
