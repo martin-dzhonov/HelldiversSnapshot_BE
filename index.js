df79b718bdcc422b5134c54fb1904038f6406469
Martin Dzhonov <martin.dzhonov@gmail.com>
2025-02-25 18:05:28 +0200

    redis fix ?

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ const redisClient = redis.createClient({
     socket: {
         host: "54.164.159.200",//127.0.0.1,https://54.164.159.200
         port: 6379,
-        keepAlive: true,
         tls: {}
     }
 });
@@ -83,7 +82,7 @@ app.get('/strategem', async (req, res) => {
     };
 
     const isEmptyFilter = Object.keys(filter).length === 0;
-    const cacheKey = `strategem1_${model_name}:${isEmptyFilter ? 'all' : JSON.stringify(filter)}`;
+    const cacheKey = `strategem2_${model_name}:${isEmptyFilter ? 'all' : JSON.stringify(filter)}`;
 
     try {
         const cachedData = await redisClient.get(cacheKey);
